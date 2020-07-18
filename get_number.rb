e79167cf148b79d916bede0e331f74d14a719c24
AntoMonti <a.matviishyn@hotmail.com>
2020-07-18 20:40:37 +0300

    closing first  game and first chapter

--- get_number.rb
+++ get_number.rb
@@ -2,8 +2,44 @@
 # Written by: you!
 
 puts "Welcome to 'Get My Number!'"
+
+# Getting players name
 print "What`s your name? "
-
 input = gets
+name = input.chomp
+puts "Welcome, #{name}!"
 
-puts "Welcome, #{input}!"
+# Generating random number
+puts "I`ve got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
+
+# Trace how many chances left
+num_guesses = 0
+guessed_it = false
+
+until num_guesses == 10 || guessed_it
+  puts "You`ve got #{10 - num_guesses} guesses left"
+  print "Make a guess: "
+  guess = gets.to_i
+
+  num_guesses += 1
+
+# Logica
+  if guess < target
+    puts "Oops. Your guess was LOW."
+  elsif guess > target
+    puts "Oops. Your guess was HIGH."
+  elsif guess == target
+    puts "Good job, #{name}!"
+    puts "You guessed my number in #{num_guesses} quesses!"
+    guessed_it = true
+  end
+end
+
+unless guessed_it
+  puts "Sorry. You didn`t get my number. (It was #{target}.)"
+end
+    
+    
+
